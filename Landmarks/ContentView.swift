c78a3a9db18dd6794bf07c2e47255da45cf6bad5
João Guilherme <joaogacanuto.ada@gmail.com>
2023-05-18 21:25:55 -0300

    finishing the firsts steps

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -14,17 +14,36 @@ func change(valid:Bool) -> Bool{
 struct ContentView: View {
     var isOn: Bool = false
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                Spacer()
-                Text("California")
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+                
             }
-            .font(.subheadline)
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
