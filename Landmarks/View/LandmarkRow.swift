b50536a896612ebd8b7ad14c915c896da5b49900
João Guilherme  Araujo Canuto <joaogacanuto@gmail.com>
2023-05-19 20:29:30 -0300

    Create the Row View

---
+++ Landmarks/View/LandmarkRow.swift
@@ -0,0 +1,28 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Joao Guilherme Araujo Canuto on 19/05/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+
+    var body: some View {
+        HStack{
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
