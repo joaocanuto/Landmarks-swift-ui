abcf2b57a962316eae296e1a2773c19aca8ab496
João Guilherme  Araujo Canuto <joaogacanuto@gmail.com>
2023-05-19 21:31:02 -0300

    Mark the User’s Favorite Landmarks

--- Landmarks/View/LandmarkRow.swift
+++ Landmarks/View/LandmarkRow.swift
@@ -16,14 +16,23 @@ struct LandmarkRow: View {
                 .resizable()
                 .frame(width: 50, height: 50)
             Text(landmark.name)
+            
             Spacer()
+            
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarks[1])
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
             .previewLayout(.fixed(width: 300, height: 700))
     }
 }
